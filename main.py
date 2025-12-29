743e4de18220a6e55cb6417e955c67e5e141c7d7
Swamy R <swamiaws85@gmail.com>
2025-12-29 12:16:47 +0530

    added console

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ from fastapi.middleware.cors import CORSMiddleware
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+if not SUPABASE_URL or not SUPABASE_KEY:
+    raise ValueError("Supabase URL or Key not found in environment variables.")
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 app = FastAPI()
